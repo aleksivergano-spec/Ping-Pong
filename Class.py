dd1ae76927ff67dd45d53169670162e6565481a4
GabrielMeloMartins <gabrielgetulino@gmail.com>
2025-09-08 15:43:24 -0300

    Update Class.py
    change the text in ball.show_score()

--- Class.py
+++ Class.py
@@ -1,4 +1,4 @@
-
+from label import*
 from pygame import*
 class GameSprite():
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
@@ -47,7 +47,7 @@ class Ball(GameSprite):
         if self.rect.y+self.rect.width/2 > window_height or self.rect.y-self.rect.width/2 < 0:
             self.speed_y *= -1
             bounce_sound.play()
-        if self.rect.collide_rect(self, player1.rect) or self.rect.collide_rect(self, player2.rect):
+        if self.rect.colliderect(player1.rect) or self.rect.colliderect(player2.rect):
             self.speed_x *= -1
             bounce_sound.play()
     def reset(self):
@@ -62,9 +62,13 @@ class Ball(GameSprite):
         if self.rect.x > window_width:
             self.j1points += 1
             self.reset()
-    def show_score(self, window, font):
-        score1 = font.render("Score: " + str(self.j1points), True, (255, 255, 255))
-        score2 = font.render("Score: " + str(self.j2points), True, (255, 255, 255))
-        window.blit(score1, (10, 10))
-        window.blit(score2, (window.get_width() - score2.get_width() - 10, 10))
+    def show_score(self, window, fsize):
+        score1=Label(10,10, 0, 0, (0,0,0)
+        score="Score: " + str(self.j1points
+        score1.set_text(score, fsize)
+        score2=Label(window.get_width() - score2.get_width() - 10,10, 0, 0, (0,0,0)
+        score="Score: " + str(self.j2points
+        score2.set_text(score, fsize)
+        score1.draw(window)
+        score2.draw(window)
 

55f1c767993e71d76e07ee203e40d435788f6b26
GabrielMeloMartins <gabrielgetulino@gmail.com>
2025-09-29 15:54:58 -0300

    Update Class.py

--- Class.py
+++ Class.py
@@ -34,28 +34,30 @@ class Player(GameSprite):
 class Ball(GameSprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__(player_image, player_x, player_y, player_speed, width, height)
+        self.speed_x_o = 3
         self.speed_x = 3
+        self.speed_y_o = 3
         self.speed_y = 3
         self.j1points = 0
         self.j2points = 0
     def update(self,window,temps, player1, player2):
         self.bounce(window,temps, player1, player2)
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        self.rect.x += int(self.speed_x)
+        self.rect.y += int(self.speed_y)
     def bounce(self,window,temps, player1=None, player2=None):
         bounce_sound = mixer.Sound('Ping sound.wav')
         self.score(window, temps)
         if self.rect.y+self.rect.width > window.get_height() or self.rect.y<0:
-            self.speed_y *= -1
+            self.speed_y *= -1.1
             bounce_sound.play()
         if self.rect.colliderect( player1.rect) or self.rect.colliderect(player2.rect):
-            self.speed_x *= -1
+            self.speed_x *= -1.1
             bounce_sound.play()
     def reset(self, window):
         self.rect.x = window.get_width()/2-self.rect.width/2
         self.rect.y = window.get_height()/2 +self.rect.height/2
-        self.speed_x *= -1
-        self.speed_y *= 1
+        self.speed_x =self.speed_x_o
+        self.speed_y = self.speed_y_o
     def score(self, window, temps):
         if self.rect.x < 0:
             self.j2points += 1
@@ -66,7 +68,7 @@ class Ball(GameSprite):
             self.Show_Text(window,'Player 2 Loose', temps)
             self.reset(window)
     def Show_Text(self,window=display.set_mode((0,0)), text=str(),temps=int()):
-        Text=Label(window.get_width()/2,window.get_height()/2,0,0,(0,0,0))
+        Text=Label(0,window.get_height()/2,0,0,(0,0,0))
         Text.set_text(text,100)
         Text.draw(window)
         display.update()
